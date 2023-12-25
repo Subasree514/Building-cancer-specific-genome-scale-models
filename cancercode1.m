66290ffd0a28d979a1f8759cb2f7c567fbfab982
Subasree Sridhar <32142237+subasreess@users.noreply.github.com>
2023-12-25 16:04:58 +0100

    Add files via upload

---
+++ cancercode1.m
@@ -0,0 +1,59 @@
+load('model_final.mat');
+load('rxnTisMatccle_september.mat');
+run ccleinformation.m;
+%%
+cell_t={'ACH-000651'%sw620
+'ACH-000199'%hs255t
+'ACH-000443'%ovkate
+'ACH-000494'%oele
+'ACH-000374'%hcc1143
+'ACH-000642'%hmel
+'ACH-000681'%a549
+'ACH-000758'};%mkn74
+%%
+cellids_t=contains(ccleconditions,cell_t);
+c_id_t=find(cellids_t==1);
+%%
+epsilon=getCobraSolverParams('LP', 'feasTol')*100;
+c1_t=find(rxnTisMatccle(:,c_id_t(1))==1);
+c2_t=find(rxnTisMatccle(:,c_id_t(2))==1);
+c3_t=find(rxnTisMatccle(:,c_id_t(3))==1);
+c4_t=find(rxnTisMatccle(:,c_id_t(4))==1);
+c5_t=find(rxnTisMatccle(:,c_id_t(5))==1);
+c6_t=find(rxnTisMatccle(:,c_id_t(6))==1);
+c7_t=find(rxnTisMatccle(:,c_id_t(7))==1);
+c8_t=find(rxnTisMatccle(:,c_id_t(8))==1);
+%% swiftcore
+order=1:length(model.rxns);
+all=order';
+smat=model.S;
+revs=find(model.lb<0);
+irrevs=setdiff(all,revs);
+run penaltyweights.m;
+full=order';
+full(revs)=1;
+full(irrevs)=0;
+%consistent = swiftcc(model.S,full,'gurobi');
+%swiftcoreconsistent=intersect(consistent,rxncore);
+result = blocked(model.S,full,'gurobi');
+modelnewS.S=model.S;
+modelnewS.c=model.c;
+modelnewS.lb=model.lb;
+modelnewS.ub=model.ub;
+modelnewS.rxns=model.rxns;
+modelnewS.rev=full;
+component = partition(modelnewS,'gurobi', 'swift');
+new=result.x(length(model.mets):(length(model.mets)+length(model.rxns)))
+blockednumbers=find(new==-1);
+newfull(blockednumbers)=1;
+unblockednumbers=setdiff(all,blockednumbers);
+newfull(unblockednumbers)=0;
+flux = core(modelnewS,newfull,weights,'gurobi');
+find(flux==0);
+epsilon=getCobraSolverParams('LP', 'feasTol')*100;
+%%
+corematrix = {c1_t;c2_t;c3_t;c4_t;c5_t;c6_t;c7_t;c8_t};
+for i = 1:length(corematrix)
+corem1=[731;773;954;2271;2277;7488;5190;corematrix{i,1}];
+[modelswiftcore(i), reconInd, LP] = swiftcore(model,corem1,weights,epsilon,false,'gurobi');
+end
---
+++ cancercode2.m
@@ -0,0 +1,71 @@
+%%
+modelb1=modelswiftcore(1)
+modelb2=modelswiftcore(2)
+modelb3=modelswiftcore(3)
+modelb4=modelswiftcore(4)
+modelb5=modelswiftcore(5)
+modelb6=modelswiftcore(6)
+modelb7=modelswiftcore(7)
+modelb8=modelswiftcore(8)
+%%
+modelb1.lb=modelb1.lb.*1e3;
+modelb1.ub=modelb1.ub.*1e3;
+model1=modelb1;
+dmemconstraints
+dmemtransports
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model1,'S')
+modelSW620=model1Out;
+%%
+modelb2.lb=modelb2.lb.*1e3;
+modelb2.ub=modelb2.ub.*1e3;
+model1=modelb2;
+dmemconstraints
+dmemtransports
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model1,'S')
+modelHS255T=model1Out;
+%%
+modelb3.lb=modelb3.lb.*1e3;
+modelb3.ub=modelb3.ub.*1e3;
+model1=modelb3;
+dmemconstraints
+dmemtransports
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model1,'S')
+modelOVKATE=model1Out;
+%%
+modelb4.lb=modelb4.lb.*1e3;
+modelb4.ub=modelb4.ub.*1e3;
+model1=modelb4;
+dmemconstraints
+dmemtransports
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model1,'S')
+modelOELE=model1Out;
+%%
+modelb5.lb=modelb5.lb.*1e3;
+modelb5.ub=modelb5.ub.*1e3;
+model1=modelb5;
+dmemconstraints
+dmemtransports
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model1,'S')
+modelHCC1143=model1Out;
+%%
+modelb6.lb=modelb6.lb.*1e3;
+modelb6.ub=modelb6.ub.*1e3;
+model1=modelb6;
+dmemconstraints
+dmemtransports
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model1,'S')
+modelHMEL=model1Out;
+%%
+modelb7.lb=modelb7.lb.*1e3;
+modelb7.ub=modelb7.ub.*1e3;
+model3=modelb7;
+memhuman
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model3,'S')
+modelA549=model1Out;
+%%
+modelb8.lb=modelb8.lb.*1e3;
+modelb8.ub=modelb8.ub.*1e3;
+model3=modelb8;
+memhuman
+[model1Out, removedRxnInd, keptRxnInd] = checkDuplicateRxn(model3,'S')
+modelMKN28=model1Out;
